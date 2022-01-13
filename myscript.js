62edac4d78a691126f5c68a999ec5827251e2ec8
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-01-13 14:56:38 +0100

    allego img con vue.js

--- myscript.js
+++ myscript.js
@@ -2,7 +2,8 @@ let app = new Vue(
     {
         el: '#container',
         data: {
-            message: 'Benvenuto in...'
+            message: 'Benvenuto in...',
+            image: 'https://www.netfarm.it/web/image/4973/vuejs.png'
         }
     },
 );
